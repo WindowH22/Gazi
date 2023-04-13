1a661e4a0c428d8185b1b8d9cae708141bb15b00
WindowH22 <dlckdgml1235@naver.com>
2023-04-13 21:38:33 +0900

    fix: Fix about JpaRepository

--- src/main/java/com/example/gazi/repository/ReplyRepository.java
+++ src/main/java/com/example/gazi/repository/ReplyRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ReplyRepository extends JpaRepository<Long,Reply> {
+public interface ReplyRepository extends JpaRepository<Reply, Long> {
 }
