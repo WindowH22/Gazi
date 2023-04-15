b05c14499c1df10033a20e0597f3e6d3bbadf5ec
WindowH22 <dlckdgml1235@naver.com>
2023-04-15 11:58:53 +0900

    feat: Create CustomUserDetailsService - Issue #3

---
+++ src/main/java/com/example/gazi/service/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.example.gazi.service;
+
+import com.example.gazi.domain.Member;
+import com.example.gazi.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return memberRepository.findByEmail(username)
+                .map(this::createUserDetails)
+                .orElseThrow(() -> new UsernameNotFoundException("해당 유저를 찾을 수 없습니다."));
+    }
+
+    private UserDetails createUserDetails(Member member) {
+        GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(member.getRole().toString());
+        return new User(String.valueOf(member.getEmail()), member.getPassword(), Collections.singleton(grantedAuthority));
+    }
+}
