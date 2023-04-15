def0b9681ee73871b86cfe6e86c446ad89c550a7
WindowH22 <dlckdgml1235@naver.com>
2023-04-15 14:57:00 +0900

    conf : Add springSecurity - Issue #3

--- src/main/java/com/example/gazi/config/SecurityConfig.java
+++ src/main/java/com/example/gazi/config/SecurityConfig.java
@@ -1,13 +1,44 @@
 package com.example.gazi.config;
 
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@RequiredArgsConstructor
+@EnableWebSecurity
 @Configuration
 public class SecurityConfig {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final RedisTemplate redisTemplate;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        return
+                http.csrf().disable()
+                        .sessionManagement(session ->
+                                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                        )
+
+
+                        .headers()
+                        .frameOptions().disable().and()
+                        .authorizeRequests()
+                        .requestMatchers("/api/v1/members/signup", "/api/v1/members/login").permitAll()
+                        .and()
+                        .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider, redisTemplate), UsernamePasswordAuthenticationFilter.class)
+                        .build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
