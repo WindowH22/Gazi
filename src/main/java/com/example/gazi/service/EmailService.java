640c129b66f7e2d992b717aac92ab3540baf3331
WindowH22 <dlckdgml1235@naver.com>
2023-04-19 20:02:20 +0900

    feat: emailService 구현 #4

---
+++ src/main/java/com/example/gazi/service/EmailService.java
@@ -0,0 +1,5 @@
+package com.example.gazi.service;
+
+public interface EmailService {
+    String sendSimpleMessage(String to)throws Exception;
+}
---
+++ src/main/java/com/example/gazi/service/EmailServiceImpl.java
@@ -0,0 +1,72 @@
+package com.example.gazi.service;
+
+import jakarta.mail.Message;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
+@RequiredArgsConstructor
+@Service
+public class EmailServiceImpl implements EmailService{
+
+    private final JavaMailSender emailSender;
+
+    public static final String ePw = createKey();
+
+    private MimeMessage createMessage(String to)throws Exception{
+        System.out.println("보내는 대상 : "+ to);
+        System.out.println("인증 번호 : "+ePw);
+        MimeMessage  message = emailSender.createMimeMessage();
+
+        message.addRecipients(Message.RecipientType.TO, to);//보내는 대상
+        message.setSubject("이메일 인증 테스트");//제목
+
+        String msgg="";
+        msgg+= "<div style='margin:20px;'>";
+        msgg+= "<h1> 안녕하세요 가지입니다. </h1>";
+        msgg+= "<br>";
+        msgg+= "<p>아래 코드를 복사해 입력해주세요<p>";
+        msgg+= "<br>";
+        msgg+= "<p>감사합니다.<p>";
+        msgg+= "<br>";
+        msgg+= "<div align='center' style='border:1px solid black; font-family:verdana';>";
+        msgg+= "<h3 style='color:blue;'>회원가입 인증 코드입니다.</h3>";
+        msgg+= "<div style='font-size:130%'>";
+        msgg+= "CODE : <strong>";
+        msgg+= ePw+"</strong><div><br/> ";
+        msgg+= "</div>";
+        message.setText(msgg, "utf-8", "html");//내용
+        message.setFrom(new InternetAddress("changheeckdgml@gmail.com","leechanghee"));//보내는 사람
+
+        return message;
+    }
+
+    public static String createKey() {
+        StringBuffer key = new StringBuffer();
+        Random rnd = new Random();
+
+        for (int i = 0; i < 4; i++) { // 인증코드 8자리
+            key.append(rnd.nextInt(10));
+        }
+        return key.toString();
+    }
+
+
+    @Override
+    public String sendSimpleMessage(String to) throws Exception {
+        // TODO Auto-generated method stub
+        MimeMessage message = createMessage(to);
+        try{//예외처리
+            emailSender.send(message);
+        }catch(MailException es){
+            es.printStackTrace();
+            throw new IllegalArgumentException();
+        }
+        return ePw;
+    }
+}
