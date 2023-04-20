82f3c100c507482f4cbcabfff0c021f330b045e0
WindowH22 <dlckdgml1235@naver.com>
2023-04-20 09:35:58 +0900

    fix: email로 검사하는 오류 수정

--- src/main/java/com/example/gazi/service/MemberServiceImpl.java
+++ src/main/java/com/example/gazi/service/MemberServiceImpl.java
@@ -58,7 +58,7 @@ public class MemberServiceImpl implements MemberService {
 
     @Override
     public ResponseEntity<Body> checkNickName(String nickName) {
-        if(memberRepository.existsByEmail(nickName)){
+        if(memberRepository.existsByNickName(nickName)){
             return response.fail("이미 존재하는 닉네임입니다.",HttpStatus.UNAUTHORIZED);
         }else{
             return response.success("사용가능한 닉네임입니다.");
