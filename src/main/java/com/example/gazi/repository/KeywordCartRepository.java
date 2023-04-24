ecc8eec0725478d5fcb4b721f239c855a3fab44d
WindowH22 <dlckdgml1235@naver.com>
2023-04-24 10:29:35 +0900

    feat: Add JpaMethod #4

--- src/main/java/com/example/gazi/repository/KeywordCartRepository.java
+++ src/main/java/com/example/gazi/repository/KeywordCartRepository.java
@@ -1,9 +1,16 @@
 package com.example.gazi.repository;
 
+import com.example.gazi.domain.Cart;
+import com.example.gazi.domain.Keyword;
 import com.example.gazi.domain.KeywordCart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface KeywordCartRepository extends JpaRepository<KeywordCart, Long> {
+    List<KeywordCart> findAllByCart(Cart cart);
+
+    Boolean existsByCartAndKeyword(Cart cart, Keyword keyword);
 }
