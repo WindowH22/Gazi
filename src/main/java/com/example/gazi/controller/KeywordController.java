f2abe364eaae898f32dadd650a242018c18d48f3
WindowH22 <dlckdgml1235@naver.com>
2023-04-24 10:22:41 +0900

    feat: KeywordController 기능구현 #5

---
+++ src/main/java/com/example/gazi/controller/KeywordController.java
@@ -0,0 +1,31 @@
+package com.example.gazi.controller;
+
+import com.example.gazi.dto.RequestKeywordCartDto;
+import com.example.gazi.dto.RequestKeywordDto;
+import com.example.gazi.dto.Response.Body;
+import com.example.gazi.service.KeywordService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@CrossOrigin
+@RequestMapping("/api/v1/keyword")
+@RestController
+public class KeywordController {
+
+    private final KeywordService keywordService;
+    //관심 키워드 등록
+    @PostMapping("interest-keyword")
+    public ResponseEntity<Body> interestKeyword(@RequestBody List<RequestKeywordCartDto> dto){
+       return keywordService.interestKeyword(dto);
+    }
+
+    //키워드 추가
+    @PostMapping("/add-keyword")
+    public ResponseEntity<Body> addKeyword(@RequestBody RequestKeywordDto dto){
+        return keywordService.addKeyword(dto);
+    }
+}
