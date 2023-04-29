7e55d83b8f9db34e6c06e791b632b6d12908b9ab
WindowH22 <dlckdgml1235@naver.com>
2023-04-30 00:07:04 +0900

    feat: Add PostController #12

---
+++ src/main/java/com/example/gazi/controller/PostController.java
@@ -0,0 +1,39 @@
+package com.example.gazi.controller;
+
+import com.example.gazi.dto.RequestPostDto;
+import com.example.gazi.dto.Response.Body;
+import com.example.gazi.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/post")
+@RestController
+public class PostController {
+
+    private final PostService postService;
+
+    @PostMapping("/topPost")
+    public ResponseEntity<Body> addPost(@RequestPart RequestPostDto.addPostDto dto, @RequestPart List<MultipartFile> files){
+        return postService.addPost(dto,files);
+    }
+
+    @GetMapping("/topPost")
+    public ResponseEntity<Body> getPost(@RequestParam Long postId){
+        return postService.getPost(postId);
+    }
+
+    @PutMapping("/topPost")
+    public ResponseEntity<Body> updatePost(@RequestParam Long postId,@RequestPart RequestPostDto.updatePostDto dto, @RequestPart List<MultipartFile> files){
+        return postService.updatePost(postId,dto,files);
+    }
+
+    @DeleteMapping("/topPost")
+    public ResponseEntity<Body> deletePost(@RequestParam Long postId){
+        return postService.deletePost(postId);
+    }
+}
