b79d98f4ffa565bba8936573099ba82dd99168ef
WindowH22 <dlckdgml1235@naver.com>
2023-05-03 10:45:35 +0900

    feat: Add Getter annotation #11

--- src/main/java/com/example/gazi/domain/AuditingFields.java
+++ src/main/java/com/example/gazi/domain/AuditingFields.java
@@ -3,17 +3,17 @@ package com.example.gazi.domain;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
+@Getter
 @Setter
 @ToString
 @EntityListeners(AuditingEntityListener.class)
