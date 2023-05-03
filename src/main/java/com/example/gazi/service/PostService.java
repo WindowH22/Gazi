205e99b45571f2a2f393b1bb69abe028e9ece23f
WindowH22 <dlckdgml1235@naver.com>
2023-05-03 10:31:08 +0900

    feat: Refactor PostService #12 - addPost 매개변수 변경 - getPost -> getTopPost 이름 변경 - getPost, getPostByLocation 추가

--- src/main/java/com/example/gazi/service/PostService.java
+++ src/main/java/com/example/gazi/service/PostService.java
@@ -3,17 +3,23 @@ package com.example.gazi.service;
 import com.example.gazi.dto.RequestPostDto;
 import com.example.gazi.dto.RequestPostDto.addPostDto;
 import com.example.gazi.dto.Response.Body;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 public interface PostService {
-    ResponseEntity<Body> addPost(addPostDto dto, List<MultipartFile> fileList);
+
+    ResponseEntity<Body> addPost(addPostDto dto, List<MultipartFile> fileList, MultipartFile thumbNail);
 
     ResponseEntity<Body> updatePost(Long postId, RequestPostDto.updatePostDto dto, List<MultipartFile> multipartFiles);
 
     ResponseEntity<Body> deletePost(Long postId);
 
-    ResponseEntity<Body> getPost(Long postId);
+    ResponseEntity<Body> getTopPost(Long postId);
+
+    ResponseEntity<Body> getPost();
+
+    ResponseEntity<Body> getPostByLocation(Double minLat, Double minLon, Double maxLat, Double maxLon, Double curX, Double curY, Pageable pageable);
 }
