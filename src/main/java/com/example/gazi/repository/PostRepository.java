da661b7489dc4de4262660e8ec789933978f6bec
WindowH22 <dlckdgml1235@naver.com>
2023-05-03 10:35:11 +0900

    feat: Add Jpa method  #17

--- src/main/java/com/example/gazi/repository/PostRepository.java
+++ src/main/java/com/example/gazi/repository/PostRepository.java
@@ -1,9 +1,18 @@
 package com.example.gazi.repository;
 
 import com.example.gazi.domain.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+    @Override
+    Page<Post> findAll(Pageable pageable);
+
+    @Query("SELECT p FROM Post p WHERE p.latitude >= :minLat AND p.longitude >= :minLon AND p.latitude <= :maxLat AND p.longitude <= :maxLon")
+    Page<Post> findAllByLocation(@Param("minLat") Double minLat, @Param("minLon") Double minLon, @Param("maxLat") Double maxLat, @Param("maxLon") Double maxLon, Pageable pageable);
 }
