dbdeb2d86f91278aa00bbf016cd1896c27ba9a6f
WindowH22 <dlckdgml1235@naver.com>
2023-05-03 10:36:38 +0900

    feat: Refactor PostController  #12 - addPost 매개변수 추가 ( 썸네일용) - getPost -> getTopPost로 이름변경

--- src/main/java/com/example/gazi/controller/PostController.java
+++ src/main/java/com/example/gazi/controller/PostController.java
@@ -4,6 +4,9 @@ import com.example.gazi.dto.RequestPostDto;
 import com.example.gazi.dto.Response.Body;
 import com.example.gazi.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -18,20 +21,19 @@ public class PostController {
     private final PostService postService;
 
     @PostMapping("/topPost")
-    public ResponseEntity<Body> addPost(@RequestPart RequestPostDto.addPostDto dto, @RequestPart(required = false) List<MultipartFile> files) {
-        return postService.addPost(dto, files);
+    public ResponseEntity<Body> addPost(@RequestPart RequestPostDto.addPostDto dto, @RequestPart(required = false) List<MultipartFile> files, @RequestPart(required = false) MultipartFile thumbnail) {
+        return postService.addPost(dto, files,thumbnail);
     }
 
     @GetMapping("/topPost")
-    public ResponseEntity<Body> getPost(@RequestParam Long postId) {
-        return postService.getPost(postId);
+    public ResponseEntity<Body> getTopPost(@RequestParam Long postId) {
+        return postService.getTopPost(postId);
     }
 
     @PutMapping("/topPost")
     public ResponseEntity<Body> updatePost(@RequestParam Long postId, @RequestPart RequestPostDto.updatePostDto dto, @RequestPart(required = false) List<MultipartFile> files) {
         return postService.updatePost(postId, dto, files);
     }
-
     @DeleteMapping("/topPost")
     public ResponseEntity<Body> deletePost(@RequestParam Long postId) {
         return postService.deletePost(postId);
