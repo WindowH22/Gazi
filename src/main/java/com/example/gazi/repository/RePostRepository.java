55efbd145e89176fe9a9da27491ad3dabf57ee7a
WindowH22 <dlckdgml1235@naver.com>
2023-05-04 14:37:03 +0900

    feat: Change type from List  #18

--- src/main/java/com/example/gazi/repository/RePostRepository.java
+++ src/main/java/com/example/gazi/repository/RePostRepository.java
@@ -1,13 +1,13 @@
 package com.example.gazi.repository;
 
 import com.example.gazi.domain.Post;
-import com.example.gazi.domain.RePost;
+import com.example.gazi.domain.Repost;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface RePostRepository extends JpaRepository<RePost, Long> {
-    List<RePost> findAllByPost(Post post);
+public interface RePostRepository extends JpaRepository<Repost, Long> {
+    Page<Repost> findAllByPost(Post post, Pageable pageable);
 }
