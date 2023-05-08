cc259e0ae34540745191a56a295c3f48de6773d0
WindowH22 <dlckdgml1235@naver.com>
2023-05-08 12:57:25 +0900

    feat: 이너클래스로 관리 post, repost #12

--- src/main/java/com/example/gazi/dto/RequestReportDto.java
+++ src/main/java/com/example/gazi/dto/RequestReportDto.java
@@ -3,8 +3,16 @@ package com.example.gazi.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
+
 public class RequestReportDto {
-    private Long postId;
+    @Getter
+    @Setter
+    public static class reportPostDto{
+        private Long postId;
+    }
+    @Getter
+    @Setter
+    public static class reportRepostDto{
+        private Long repostId;
+    }
 }
--- src/main/java/com/example/gazi/dto/ResponseFileDto.java
+++ src/main/java/com/example/gazi/dto/ResponseFileDto.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-public class ResponseFilePostDto {
+public class ResponseFileDto {
 
     private String fileName;
     private String fileUrl;
