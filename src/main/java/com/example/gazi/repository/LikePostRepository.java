fc994bc3cd2892cca7b88dd218635d3521f0b6e6
WindowH22 <dlckdgml1235@naver.com>
2023-05-08 13:20:26 +0900

    feat: Add countByRepost,countByPost  #16

--- src/main/java/com/example/gazi/repository/LikePostRepository.java
+++ src/main/java/com/example/gazi/repository/LikePostRepository.java
@@ -1,19 +1,22 @@
 package com.example.gazi.repository;
 
 import com.example.gazi.domain.LikePost;
+import com.example.gazi.domain.Post;
+import com.example.gazi.domain.Repost;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface LikePostRepository extends JpaRepository<LikePost, Long> {
     Optional<LikePost> findByLikeIdAndPostId(Long likeId, Long postId);
-
-    List<LikePost> findALLByLikeIdAndPostId(Long id, Long postId);
 
     boolean existsByLikeIdAndPostId(Long id, Long id1);
 
     boolean existsByLikeIdAndRepostId(Long id, Long id1);
 
     Optional<LikePost> findByLikeIdAndRepostId(Long likeId, Long repostId);
+
+    Long countByRepost(Repost repost);
+
+    Long countByPost(Post post);
 }
