a0a8c17cbf071bc5e5c94531dbaa6002e3800f3c
WindowH22 <dlckdgml1235@naver.com>
2023-05-08 13:00:29 +0900

    refactor: 기존 RepostDto 수정하여 PostListDto 생성 #18

---
+++ src/main/java/com/example/gazi/dto/ResponsePostListDto.java
@@ -0,0 +1,55 @@
+package com.example.gazi.dto;
+
+import com.example.gazi.domain.Post;
+import com.example.gazi.domain.Repost;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@Builder
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class ResponsePostListDto {
+
+    private String distance;
+    private String content;
+    private List<ResponseFileDto> fileList;
+    private String nickName;
+    private String time;
+    private boolean isLike;
+    private Long likeCount;
+    private boolean isReport;
+    private List<Long> keywordIdList;
+
+    public static ResponsePostListDto toDto(Post post, String time, String distance, List<ResponseFileDto> fileList, Long likeCount,boolean isLike, boolean isReport,List<Long> keywordIdList){
+        return ResponsePostListDto.builder()
+                .content(post.getContent())
+                .distance(distance)
+                .fileList(fileList)
+                .nickName(post.getMember().getNickName())
+                .time(time)
+                .isLike(isLike)
+                .likeCount(likeCount)
+                .isReport(isReport)
+                .keywordIdList(keywordIdList)
+                .build();
+    }
+    public static ResponsePostListDto toDto(Repost repost, String time, String distance, List<ResponseFileDto> fileList, Long likeCount, boolean isLike, boolean isReport, List<Long> keywordIdList){
+        return ResponsePostListDto.builder()
+                .content(repost.getContent())
+                .distance(distance)
+                .fileList(fileList)
+                .nickName(repost.getMember().getNickName())
+                .time(time)
+                .isLike(isLike)
+                .likeCount(likeCount)
+                .isReport(isReport)
+                .keywordIdList(keywordIdList)
+                .build();
+    }
+
+}
