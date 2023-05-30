c1c5a28cd2d077c2a3c17b10d8c190dab54a4516
WindowH22 <dlckdgml1235@naver.com>
2023-05-30 20:33:00 +0900

    feat: Add 서울공공데이터 불러오는 서비스 분리 # 23

---
+++ src/main/java/com/example/gazi/service/OpenApiService.java
@@ -0,0 +1,9 @@
+package com.example.gazi.service;
+
+import org.json.JSONArray;
+
+import java.io.IOException;
+
+public interface OpenApiService {
+    JSONArray getJsonArray() throws IOException;
+}
