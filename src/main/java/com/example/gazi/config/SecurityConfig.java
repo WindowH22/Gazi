f8db86c1fb520ec75cadb08a399d86fe6d04c8f4
WindowH22 <dlckdgml1235@naver.com>
2023-06-21 11:51:33 +0900

    feat: requestMatchers 추가 - 닉네임 중복체크

--- src/main/java/com/example/gazi/config/SecurityConfig.java
+++ src/main/java/com/example/gazi/config/SecurityConfig.java
@@ -25,7 +25,6 @@ public class SecurityConfig {
     private final CustomOauth2UserService userService;
     private final OAuth2SuccessHandler successHandler;
 
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
@@ -35,14 +34,13 @@ public class SecurityConfig {
                                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                         )
 
-
                         .headers()
                         .frameOptions().disable().and()
                         .logout().disable() // 로그아웃 사용 X
                         .formLogin().disable() // 폼 로그인 사용 X
 
                         .authorizeRequests()
-                        .requestMatchers("/api/v1/member/signup", "/api/v1/member/login","/api/v1/member/email-confirm","/api/v1/member/reissue").permitAll()
+                        .requestMatchers("/api/v1/member/signup", "/api/v1/member/login","/api/v1/member/email-confirm","/api/v1/member/reissue","/api/v1/member/check-nickname").permitAll()
                         .anyRequest().authenticated() // 나머지 요청들은 모두 인증 절차 수행해야함
 
                         .and()
