0836d2f09c8a46203246336816c644fce328293c
WindowH22 <dlckdgml1235@naver.com>
2023-06-25 20:10:52 +0900

    feat: RequestFCMNotificationDto dto 구현 #30 알림 구현을 위한 dto 구현

--- src/main/java/com/example/gazi/dto/RequestFCMNotificationDto.java
+++ src/main/java/com/example/gazi/dto/RequestFCMNotificationDto.java
@@ -1,8 +1,11 @@
 package com.example.gazi.dto;
 
+import com.example.gazi.domain.Member;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.Set;
 
 @Getter
 @NoArgsConstructor
@@ -10,11 +13,13 @@ public class RequestFCMNotificationDto {
     private Long targetUserId;
     private String title;
     private String body;
+    private Set<Member> memberList;
 
     @Builder
-    public RequestFCMNotificationDto(Long targetUserId, String title, String body) {
+    public RequestFCMNotificationDto(Long targetUserId, String title, String body, Set<Member> memberList) {
         this.targetUserId = targetUserId;
         this.title = title;
         this.body = body;
+        this.memberList = memberList;
     }
 }
