0d1a7001e6a3e331b057aa0f8cc0d4d3511898bf
WindowH22 <dlckdgml1235@naver.com>
2023-07-07 21:49:34 +0900

    feat: Change unique

--- src/main/java/com/example/gazi/domain/Member.java
+++ src/main/java/com/example/gazi/domain/Member.java
@@ -23,7 +23,7 @@ public class Member extends AuditingFields {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String email;
     @Column(nullable = true)
     private String password;
