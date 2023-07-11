f189af4d45d959942a72ff4a17223912e7d809dd
WindowH22 <dlckdgml1235@naver.com>
2023-07-11 23:12:09 +0900

    feat: Add MemberServiceImplTest #35

---
+++ src/test/java/com/example/gazi/service/MemberServiceImplTest.java
@@ -0,0 +1,64 @@
+package com.example.gazi.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceImplTest {
+
+    @Test
+    void signUp() {
+    }
+
+    @Test
+    void checkEmail() {
+    }
+
+    @Test
+    void checkNickName() {
+    }
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void reissue() {
+    }
+
+    @Test
+    void logout() {
+    }
+
+    @Test
+    void getInfo() {
+    }
+
+    @Test
+    void deleteMember() {
+    }
+
+    @Test
+    void changeNickName() {
+    }
+
+    @Test
+    void validateHandling() {
+    }
+
+    @Test
+    void getFirebaseAccessToken() {
+    }
+
+    @Test
+    void changeNotificationByKeyword() {
+    }
+
+    @Test
+    void changeNotificationByRepost() {
+    }
+
+    @Test
+    void changeNotificationByLike() {
+    }
+}
