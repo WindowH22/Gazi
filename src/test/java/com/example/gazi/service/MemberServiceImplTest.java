50f75f140518921c37a0a063c94ebc79a4c46868
WindowH22 <dlckdgml1235@naver.com>
2023-07-13 23:47:47 +0900

    feat: Add 회원가입 테스트 #35

--- src/test/java/com/example/gazi/service/MemberServiceImplTest.java
+++ src/test/java/com/example/gazi/service/MemberServiceImplTest.java
@@ -1,13 +1,44 @@
 package com.example.gazi.service;
 
+import com.example.gazi.domain.Member;
+import com.example.gazi.dto.RequestMember;
+import com.example.gazi.repository.MemberRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@Transactional
 class MemberServiceImplTest {
 
+    @Autowired
+    MemberServiceImpl memberService;
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
     @Test
+    @DisplayName("회원가입")
     void signUp() {
+        // given
+        RequestMember.SignUp dto = new RequestMember.SignUp();
+        dto.setEmail("dlckdgml35@gmail.com");
+        dto.setPassword("kkkllll");
+        dto.setNickName("testnick");
+
+        // when
+        Member member = memberService.signUp(dto);
+
+        // then
+        assertEquals(member,memberRepository.findByEmail(dto.getEmail()).get());
+
     }
 
     @Test
