5aea6da87a37157e63f2074059ff42aaa9bf6782
WindowH22 <dlckdgml1235@naver.com>
2023-07-14 23:47:21 +0900

    feat: Add setUp()메서드 추가 #35

--- src/test/java/com/example/gazi/service/MemberServiceImplTest.java
+++ src/test/java/com/example/gazi/service/MemberServiceImplTest.java
@@ -2,16 +2,18 @@ package com.example.gazi.service;
 
 import com.example.gazi.domain.Member;
 import com.example.gazi.dto.RequestMember;
+import com.example.gazi.dto.Response;
 import com.example.gazi.repository.MemberRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @Transactional
@@ -23,15 +25,23 @@ class MemberServiceImplTest {
     MemberRepository memberRepository;
     @Autowired
     PasswordEncoder passwordEncoder;
+    @Autowired
+    Response response;
+
+    RequestMember.SignUp setUp(){
+        RequestMember.SignUp dto = new RequestMember.SignUp();
+        dto.setEmail("dlckdgml35@gmail.com");
+        dto.setPassword("kkkllll");
+        dto.setNickName("testnick");
+
+        return dto;
+    }
 
     @Test
     @DisplayName("회원가입")
     void signUp() {
         // given
-        RequestMember.SignUp dto = new RequestMember.SignUp();
-        dto.setEmail("dlckdgml35@gmail.com");
-        dto.setPassword("kkkllll");
-        dto.setNickName("testnick");
+        RequestMember.SignUp dto = setUp();
 
         // when
         Member member = memberService.signUp(dto);
@@ -45,24 +55,22 @@ class MemberServiceImplTest {
     @DisplayName("이메일 중복 테스트")
     void checkEmail() {
         // given
-        RequestMember.SignUp dto1 = new RequestMember.SignUp();
-        dto1.setEmail("dlckdgml35@gmail.com");
-        dto1.setPassword("kkkllll");
-        dto1.setNickName("testnick");
-
+        RequestMember.SignUp dto1 = setUp();
         RequestMember.SignUp dto2 = new RequestMember.SignUp();
         dto2.setEmail("dlckdgml35@gmail.com");
         dto2.setPassword("ddd");
         dto2.setNickName("fadf");
 
         // when
-        Member member1 =  memberService.signUp(dto1);
         try{
+            Member member1 =  memberService.signUp(dto1);
             Member member2 =  memberService.signUp(dto2);
-        }catch (IllegalStateException e){
-            return;
         }
         // then
+        catch (IllegalStateException e){
+            return;
+        }
+
 
     }
 
