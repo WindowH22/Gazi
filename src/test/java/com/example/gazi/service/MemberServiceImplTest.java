261e3fbd0aad2f6fa127202b7cdf26a620edc3e1
WindowH22 <dlckdgml1235@naver.com>
2023-07-23 14:52:21 +0900

    feat: 파이어베이스 토큰 등록 실패 테스트 코드 구현 #35

--- src/test/java/com/example/gazi/service/MemberServiceImplTest.java
+++ src/test/java/com/example/gazi/service/MemberServiceImplTest.java
@@ -329,6 +329,29 @@ class MemberServiceImplTest {
     }
 
     @Test
+    @DisplayName("존재하지 않는 이메일로 인한 파이어베이스 토큰 등록 실패")
+    void failGetFirebaseAccessToken() {
+        //given
+        RequestMember.SignUp signUp = signUpSetUp();
+        memberService.signUp(signUp);
+        RequestMember.Login login = loginSetUp();
+        memberService.login(login);
+
+        SecurityContext context = SecurityContextHolder.getContext();
+        context.setAuthentication(new UsernamePasswordAuthenticationToken(login.getEmail()+"fail", login.getPassword()));
+
+        RequestMember.FirebaseToken firebaseToken = new RequestMember.FirebaseToken();
+        firebaseToken.setFireBaseToken("fireBaseTokenTest");
+
+        //when
+        ResponseEntity<Response.Body> responseEntity = memberService.getFirebaseAccessToken(firebaseToken);
+
+        //then
+        assertEquals(HttpStatus.NOT_FOUND,responseEntity.getStatusCode());
+
+    }
+
+    @Test
     void changeNotificationByKeyword() {
     }
 
